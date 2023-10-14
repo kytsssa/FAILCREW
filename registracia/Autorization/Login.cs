bcc4e5586102fa1e3d55f630434bcd34fd78a369
kytsssa <146727825+kytsssa@users.noreply.github.com>
2023-10-14 15:29:37 +0400

    Add files via upload

---
+++ registracia/Autorization/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Autorization
+{
+    internal class Login
+    {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ registracia/Autorization/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            Close();
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+        public void Reset()
+        {
+            textBoxFirstName.Text = "";
+            textBoxLastName.Text = "";
+            textBoxEmail.Text = "";
+            textBoxAddress.Text = "";
+            passwordBox1.Password = "";
+            passwordBoxConfirm.Password = "";
+        }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Введите почту";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Введите ВЕРНУЮ почту";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string firstname = textBoxFirstName.Text;
+                string lastname = textBoxLastName.Text;
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                if (passwordBox1.Password.Length == 0)
+                {
+                    errormessage.Text = "Введите пароль";
+                    passwordBox1.Focus();
+                }
+                else if (passwordBoxConfirm.Password.Length == 0)
+                {
+                    errormessage.Text = "Повторите пароль";
+                    passwordBoxConfirm.Focus();
+                }
+                else if (passwordBox1.Password != passwordBoxConfirm.Password)
+                {
+                    errormessage.Text = "Пароли должны совпадать";
+                    passwordBoxConfirm.Focus();
+                }
+                else
+                {
+                    errormessage.Text = "";
+                    string address = textBoxAddress.Text;
+                    SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Insert into Registration (FirstName,LastName,Email,Password,Address) values('" + firstname + "','" + lastname + "','" + email + "','" + password + "','" + address + "')", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    errormessage.Text = "Регистраация завершенна";
+                    Reset();
+                }
+            }
+        }
+    }
+}
+
